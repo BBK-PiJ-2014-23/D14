bd3945b7d8627f6925f4a8f5038b1b841813c1c0
Stefan E. Mayer <stefan.mayer.80@gmail.com>
2015-01-24 13:52:37 +0000

    Removed superflous method.

--- Quicksort/QuickSort.java
+++ Quicksort/QuickSort.java
@@ -30,15 +30,5 @@ public class QuickSort
             sorted.addAll(quickSort(higher));
             return sorted;
         }
-
-    }
-    public static void main(String[] args) {
-        List<Integer> list = new ArrayList<Integer>();
-        list.add(3);
-        list.add(7);
-        list.add(2);
-        list.add(9);
-        list.add(1);
-        System.out.println(quickSort(list).toString());
     }
 }
